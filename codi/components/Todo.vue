c29bfaa97e798974fb875a0d3e91ac072c9b3cb2
mutrera <116823043+mutrera@users.noreply.github.com>
2022-11-23 10:58:09 +0100

    tasques-noves

--- codi/components/Todo.vue
+++ codi/components/Todo.vue
@@ -2,86 +2,118 @@
     <v-container>
         <v-row>
             <v-col cols="12" sm="6">
-                <v-textarea 
-                label="Tasca"
-                placeholder="nova tasca"
-                outlined
-                v-moldel="laNovaTasca"
-                ></v-textarea>
-                <v-btn @click="afegirTasca()">afegir tasca</v-btn>
-            </v-col>   
+                <v-textarea
+                    label="Tasca"
+                    placeholder="p.e: fer el llit" 
+                    outlined
+                    v-model="laNovaTasca"
+                >
+                </v-textarea>
+                <v-btn 
+                    @click="afegirTasca()"
+                    color="blue"
+                    :disabled="!tincText"
+                    >afegir tasca</v-btn>
+            </v-col>
             <v-col cols="12" sm="6">
-                <v-row class="tasca" v-for="tasca in tasques">
-                    <v-col cols="8" ><h2>{{tasca.text}}</h2>
+                Tens {{tasquesFetes}} tasques fetes
+                <br>
+                i {{tasques.length - tasquesFetes}} per fer.
+                <br>
+                <br>
+                <br>
+                <v-row class="tasca" v-for="(el,index) in tasques">
+                    <v-col cols="8">
+                    
+                       <h2>{{el.text}} - {{index}}</h2>
                     </v-col>
                     <v-col cols="4">
-                        <v-checkbox v-model="tasca.feta">
-                        </v-checkbox>
+                        <v-checkbox
+                            v-model="el.feta"
+                        ></v-checkbox>
                     </v-col>
                 </v-row>
+
+
+
+
+
+
+
+
+
                 <pre>
                     {{tasques}}
                 </pre>
-            
-            </v-col>
-        
-        </v-row>
+                
+            </v-col>   
+        </v-row> 
     </v-container>
 </template>
-<style>
-.tasca{
-    background-color: papayawhip;
-    margin: 10px
-}
-
-
-</style>
 
 <script>
 export default{
     methods:{
         afegirTasca(){
-            console.log("afegir tasca...")
-            var text=this.laNovaTasca
-            var tasca={}
-            tasca.text = this.laNovaTasca
-            tasca.feta = false
-            if(this.laNovaTasca.length<=0){
-                alert("El camp tasva no pot estar buid")
+            console.log("afegint tasca...")
+
+            if(this.laNovaTasca.length <= 0){
+                alert("El camp tasca no pot estar buid")
                 return
             }
 
+            var tasca = {}
+            tasca.text = this.laNovaTasca
+            tasca.feta = false
+
             this.tasques.push(tasca)
-            this.laNovaTasca =""
+            this.laNovaTasca = ""
         }
     },
     data(){
-        return{
-            laNovaTasca:"",
+        return {  
+            laNovaTasca:"",     
             tasques:[
-            {
-                text:"Fer el llit",
-                feta:true
-            },
-            {
-                text:"Fer els deures",
-                feta:true
-            },
-            {
-                text:"Anar a comprar",
-                feta:true
-            },
-            
-        ]
+                {
+                    text:"Fer el llit",
+                    feta:true
+                },
+                {
+                    text:"Fer els deures",
+                    feta:false
+                },
+                {
+                    text:"Anar a comprar",
+                    feta:true
+                }]
         }
-      
-
-},
-   computed:{
-    tincText(){
-        if(this.laNovaTasca)
+    },
+    computed:{
+        tincText(){
+            if(this.laNovaTasca.length <=0){
+                return false
+            }
+            else{
+                return true
+            }
+        },
+        tasquesFetes(){
+            var tasquesFetesVar = []
+            this.tasques.forEach((tasca)=>{
+                if(tasca.feta == true){
+                    tasquesFetesVar.push(tasca)
+                }
+            })
+            return tasquesFetesVar.length
+        }
     }
-   }
-
 }
 </script>
+
+
+<style>
+    .tasca{
+        background-color: bisque;
+        margin-bottom: 10px;
+    }
+</style>
